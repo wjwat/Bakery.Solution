df6a6bbdb75bfd3e591ba2b625f4954b8abfa1c4
Will W <wjwat@onionslice.org>
2022-03-25 15:26:56 -0500

    Update Home Controller & Index view

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,13 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+using Bakery.Models;
+using Bakery.ViewModels;
 
 namespace Bakery.Controllers
 {
     public class HomeController : Controller
     {
 
+      private readonly BakeryContext _db;
+
+      public HomeController(BakeryContext db)
+      {
+        _db = db;
+      }
+
+
       public ActionResult Index()
       {
-        return View();
+        var HomeModel = new HomeViewModel {
+          Flavors = _db.Flavors.ToList(),
+          Treats = _db.Treats.ToList()
+        };
+        return View(HomeModel);
       }
 
     }
