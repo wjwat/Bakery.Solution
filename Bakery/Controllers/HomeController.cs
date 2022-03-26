df8fe8c9b8fe4837e06f9e0c4ba5eef2ac005526
Will W <wjwat@onionslice.org>
2022-03-26 12:35:56 -0500

    Improve splash page by sorting info if a user is logged in

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Security.Claims;
 
 using Bakery.Models;
 using Bakery.ViewModels;
@@ -18,10 +21,20 @@ namespace Bakery.Controllers
 
       public ActionResult Index()
       {
-        var HomeModel = new HomeViewModel {
-          Flavors = _db.Flavors.ToList(),
-          Treats = _db.Treats.ToList()
+        // Pull our UserId out first to use a basis for sorting the values in
+        // our tables.
+        var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        var HomeModel = new UserViewModel {
+          Flavors = _db.Flavors.ToList()
+              .OrderByDescending(f => f.User.Id == userId)
+              .ToList(),
+          Treats = _db.Treats
+              .OrderByDescending(t => t.User.Id == userId)
+              .ToList(),
+          UserId = userId
         };
+
         return View(HomeModel);
       }
 
